d8c8aaadc20b8e200e375c297c349eb97e2ed12b
Matt Nawrocki <mjnrock@umich.edu>
2020-01-03 23:38:31 -0500

    init

--- client/src/contexts/Lumen.js
+++ client/src/contexts/Lumen.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Lux from "@lespantsfancy/lux";
 
+//TODO Refactor this file to be a (defaultValues) => React.createContext(defaultValues) situation, that still uses @Lux
+// In effect, allow the definition of initial state
+
 const Lumen = new Lux.Core.ClassDecorators.StateEvents();
 
 //* Testing variables
