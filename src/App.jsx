743c4910f83a4839e72770f00163b84e309f6e0c
Matt Nawrocki <mjnrock@umich.edu>
2020-05-01 22:34:57 -0400

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import Context from "./Context";
+import Element from "./Element";
+import Canvas from "./Canvas";
+import Input from "./Input";
+
+function useAJAX(uri) {
+    const [ info, setInfo ] = useState("");
+    useEffect(() => {
+        async function fetchInfo() {
+            console.log("CALLED")
+            let data = await fetch(uri);
+
+            setInfo(await data.json());
+        };
+
+        fetchInfo();
+    }, [ uri ]);
+
+    return JSON.stringify(info);
+    // return info.people ? (
+    //     info.people.map(p => (
+    //         <div key={ p.name }>{ p.name }</div>
+    //     ))
+    // ) : (
+    //     "Loading..."
+    // );
+}
+
+// const CTX = Context.Context;
+// console.log(CTX)
 
 function App() {
+    const CTX = useContext(Context.Context);
+    const [ lastUpdate, setLastUpdate ] = useState(CTX._lastUpdate);    // Flag variable appears to be the easiest way to force an update, but has obvious "overuse" implications
+    const [ count, setCount ] = useState(0);
+    // const info = useAJAX("http://api.open-notify.org/astros.json");
+    // const info = useAJAX("http://api.open-notify.org/iss-now.json");
+
     return (
         <div>
-            Hello
+            <div>
+                <p>{ CTX.test }</p>
+                <p>Clicks: { count }</p>
+                <button onClick={ e => { setLastUpdate(CTX.inc()); } }>Click Me</button>
+            </div>
+            <Element />
+            <Element />
+            <Canvas />
+            <Canvas />
+            <Input />
+            {/* <div>{ info }</div> */}
         </div>
     );
 }
---
+++ src/Canvas.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect, Fragment } from "react";
+
+function Canvas() {
+    const canvasRef = React.createRef();
+    const [ file, setFile ] = useState();
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        if(file) {
+            const img = new Image();
+            img.onload = () => ctx.drawImage(img, 0, 0);
+            // img.src = "./pusheen.png";  //? The image is relative to the HTML file, NOT the JSX file
+            img.src = URL.createObjectURL(file);
+        }
+    }, [ file ]);
+
+    return (
+        <Fragment>
+            <input type="file" onChange={ e => setFile(e.target.files[ 0 ]) } />
+            <canvas
+                ref={ canvasRef }
+                width={ 500 }
+                height={ 500 }
+            ></canvas>
+        </Fragment>
+    );
+}
+
+export default Canvas;
