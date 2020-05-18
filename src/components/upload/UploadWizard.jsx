bf4388bcd35d9394ce16c22700d572d559cedb81
Matt Nawrocki <mjnrock@umich.edu>
2020-05-18 19:53:50 -0400

    init

--- src/components/upload/UploadWizard.jsx
+++ src/components/upload/UploadWizard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Step, Icon, Segment } from "semantic-ui-react";
 
 import { Context } from "./../../App";
