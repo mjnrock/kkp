98f0a334f4ca40c375ef6d91eb9c44ada39f00a8
Matt Nawrocki <mjnrock@umich.edu>
2020-05-20 10:25:01 -0400

    init

---
+++ api/index copy.js
@@ -0,0 +1,293 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import multer from "multer";
+import mysql from "mysql";
+
+const DB = mysql.createPool({
+    connectionLimit: 10,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "kkp",
+});
+const APP = express();
+const PORT = 3001;
+const SERVERS = {
+    ALBUM: {
+        protocol: "http",
+        host: "192.168.86.100",
+        port: PORT,
+        endpoint: "/",
+        uri: function(extra) {
+            let scope = SERVERS.ALBUM;
+            let endpoint = `/${ scope.endpoint }/${ extra }`.replace(/[/]{2,}/g, "/");
+
+            return `${ scope.protocol }://${ scope.host }:${ scope.port }${ endpoint }`;
+        }
+    }
+};
+
+APP.use("/img", express.static(__dirname + "/data/image"));
+APP.use(express.urlencoded({ extended: true }));
+APP.use(express.json());
+APP.use(express.raw());
+APP.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //? Whatever middleware work .next() is doing is ESSENTIAL to actually making this work
+    next();
+});
+
+APP.disable("x-powered-by");
+
+//* ================= <AUTHENTICATION> =========================
+APP.post("/auth", (req, res) => {
+    const message = req.body;
+    const { email, password } = message;
+
+    console.log("/auth", message);
+
+    //TODO Make a real auth token
+    return res.send({
+        token: Date.now()
+    });
+});
+APP.post("/signup", (req, res) => {
+    const message = req.body;
+    const { email, password } = message;
+
+    console.log("/signup", message);
+
+    //TODO Make a real signup
+    return res.send({
+        token: Date.now()
+    });
+});
+//* ================= </AUTHENTICATION> =========================
+
+
+//* ================= <UPLOAD> =========================
+    // //? An example <FORM> 
+    // <form action={ `http://192.168.86.100:3001/media/upload` } method="post" encType="multipart/form-data">
+    //     <input type="file" name="avatar" />                
+    //     <input type="submit" name="upload-button" value="Upload" />
+    // </form>
+
+    const MULTER_STORAGE = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, "./data/image");
+        },
+
+        // By default, multer removes file extensions so let"s add them back
+        filename: function (req, file, cb) {
+            cb(null, `${ file.fieldname }-${ Date.now() }${ path.extname(file.originalname) }`);
+        }
+    });
+
+    APP.post("/media/upload", (req, res) => {
+        console.log("/media/upload");
+
+        let upload = multer({
+            storage: MULTER_STORAGE,
+            fileFilter: (req, file, cb) => {
+                // Accept images only
+                if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+                    req.fileValidationError = "Only image files are allowed!";
+                    return cb(new Error("Only image files are allowed!"), false);
+                }
+                cb(null, true);
+            }
+        }).single("photo");    // ("avatar") is the <input name="avatar" type="file" /> element
+
+        upload(req, res, function (err) {
+            // req.file contains information of uploaded file
+            // req.body contains information of text fields, if there were any
+
+            if (req.fileValidationError) {
+                return res.send(req.fileValidationError);
+            }
+            else if (!req.file) {
+                return res.send("Please select an image to upload");
+            }
+            else if (err instanceof multer.MulterError) {
+                return res.send(err);
+            }
+            else if (err) {
+                return res.send(err);
+            }
+
+            const postFilename = `./data/post/${ req.body.handle.toLowerCase() }.json`;
+            const postData = {
+                id: `${ req.body.handle }-${ Date.now() }`,
+                image: req.file.filename,
+            };
+
+            fs.readFile(postFilename, "utf8", (err, data) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    let posts = JSON.parse(data);
+                    
+                    posts.push(postData);
+    
+                    let json = JSON.stringify(posts);
+                    fs.writeFile(postFilename, json, () => {
+                        return res.send(postData);
+                    });
+                }
+            });
+        });
+    });
+//* ================= </UPLOAD> =========================
+
+
+//* ================= <POSTS> =========================
+    APP.post("/post/react/", (req, res) => {
+        const filepath = "./data/messages.json";
+        const message = req.body;
+        const { pid, emoji, token: user } = message;
+
+        console.log("/post/react/", message);
+
+        fs.readFile(filepath, "utf8", (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                let posts = JSON.parse(data);
+                let resPost;
+                posts.map(post => {
+                    if (post.id === pid) {
+                        post.reactions = post.reactions || [];
+                        post.reactions.push({
+                            emoji,
+                            user
+                        });
+
+                        resPost = {
+                            id: post.id,
+                            reactions: post.reactions
+                        };
+                    }
+                });
+
+                let json = JSON.stringify(posts);
+                fs.writeFile(filepath, json, () => {
+                    res.send(resPost);
+                });
+            }
+        });
+    });
+    APP.post("/post/response/", (req, res) => {
+        const filepath = "./data/messages.json";
+        const message = req.body;
+
+        console.log("/post/response/", message);
+
+        //TODO Save comments to a post
+    });
+//* ================= </POSTS> =========================
+
+APP.get("/album/:aid", (req, res) => {
+    const albumId = req.params.aid;
+    console.log(`/album/${ albumId }`);
+
+    fs.readdir(path.join(__dirname, "/data/image/"), function (err, files) {
+        if(err) {
+            return;
+        } 
+
+        const uris = files.map(file => SERVERS.ALBUM.uri(`/img/${ file }`));
+
+        res.send(uris);
+    });
+});
+
+APP.get("/user/:handle", (req, res) => {
+    const handle = req.params.handle.toLowerCase();
+    const filename = `./data/user/${ handle }.json`;
+    console.log(`/user/${handle}`);
+
+    fs.readFile(filename, function (err, buff) {
+        return res.send(buff.toString());
+    });
+});
+
+APP.get("/family/:handle", (req, res) => {
+    const handle = req.params.handle.toLowerCase();
+    const filename = `./data/family/${ handle }.json`;
+    console.log(`/family/${handle}`);
+
+    fs.readFile(filename, function (err, buff) {
+        return res.send(buff.toString());
+    });
+});
+
+APP.get("/friends/:handle", (req, res) => {
+    const handle = req.params.handle;
+    const filename = `./data/user/${ handle.toLowerCase() }.json`;
+    console.log(`/friends/${handle}`);
+
+    fs.readFile(filename, "utf8", (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            let profile = JSON.parse(data);
+            
+            return res.send(profile.friends);
+        }
+    });
+});
+
+APP.get("/post/:pid", (req, res) => {
+    const postId = req.params.pid;
+    const filename = `./data/post/${ postId.split("-")[ 0 ].toLowerCase() }.json`;
+    console.log(`/post/${postId}`);
+
+    fs.readFile(filename, "utf8", (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            let posts = JSON.parse(data);
+            let post = posts.filter(post => post.id === postId);
+    
+            if(post) {
+                return res.send(post[ 0 ]);
+            }
+
+            return res.sendStatus(404);
+        }
+    });
+});
+
+APP.get("/feed/:handle", (req, res) => {
+    const handle = req.params.handle;
+    console.log(`/feed/${handle}`);
+
+    fs.readFile(`./data/post/${ handle.toLowerCase() }.json`, function (err, buff) {
+        return res.send(buff.toString());
+    });
+});
+
+APP.get("/image/:iid", (req, res) => {
+    const imageId = "pusheen" || req.params.iid;
+    const imageExt = "png";
+    const filepath = `/data/image/${imageId}.${imageExt}`;
+    console.log(`/image/${imageId}`);
+
+    return res.sendFile(filepath, { root: __dirname });
+});
+
+
+
+APP.get("/test", (req, res) => {
+    console.log(`/test`);
+
+    DB.query("SELECT * FROM test", function (error, results, fields) {
+        return res.send(results)
+    });
+});
+
+APP.listen(PORT, () =>
+    console.log(`KiKi Pupus API listening on port ${PORT}!`),
+);
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import fs from "fs";
 import path from "path";
 import multer from "multer";
 import mysql from "mysql";
+import crypto from "crypto";
 
 const DB = mysql.createPool({
     connectionLimit: 10,
@@ -13,20 +14,6 @@ const DB = mysql.createPool({
 });
 const APP = express();
 const PORT = 3001;
-const SERVERS = {
-    ALBUM: {
-        protocol: "http",
-        host: "192.168.86.100",
-        port: PORT,
-        endpoint: "/",
-        uri: function(extra) {
-            let scope = SERVERS.ALBUM;
-            let endpoint = `/${ scope.endpoint }/${ extra }`.replace(/[/]{2,}/g, "/");
-
-            return `${ scope.protocol }://${ scope.host }:${ scope.port }${ endpoint }`;
-        }
-    }
-};
 
 APP.use("/img", express.static(__dirname + "/data/image"));
 APP.use(express.urlencoded({ extended: true }));
@@ -41,252 +28,87 @@ APP.use((req, res, next) => {
 
 APP.disable("x-powered-by");
 
-//* ================= <AUTHENTICATION> =========================
+const TOKEN_KEY = crypto.createHash("sha256").update(String(Date.now())).digest("base64").substr(0, 32);
+const IV = crypto.randomBytes(16).toString("hex").slice(0, 16);
+
+function createToken(email, password, expiration) {
+    try {
+        let text = JSON.stringify({
+            email,
+            password,
+            timestamp: Date.now(),
+            expiration
+        });
+        
+        let cipher = crypto.createCipheriv("aes-256-ctr", TOKEN_KEY, IV);
+        let encrypted = cipher.update(String(text), "utf8", "hex") + cipher.final("hex");
+
+        return encrypted;
+    } catch(e) {
+        return false;
+    }
+}
+function decryptToken(token) {
+    try {
+        let decipher = crypto.createDecipheriv("aes-256-ctr", TOKEN_KEY, IV);
+        let decrypted = decipher.update(String(token), "hex", "utf8");
+        
+        decrypted += decipher.final("utf8");
+
+        return JSON.parse(decrypted);
+    } catch(e) {
+        return false;
+    }
+}
+
+
 APP.post("/auth", (req, res) => {
     const message = req.body;
-    const { email, password } = message;
-
+    const { email, password: pwd } = message;
     console.log("/auth", message);
 
-    //TODO Make a real auth token
+    if(email && pwd) {
+        const hashPwd = crypto.createHash("sha256").update(pwd).digest("hex");
+        const token = createToken(email, hashPwd, 60 * 60 * 24);    // 24 Hours
+    
+        if(token) {
+            return res.send({
+                token,
+            });
+        }
+    }
+
     return res.send({
-        token: Date.now()
+        token: false
     });
 });
+
 APP.post("/signup", (req, res) => {
     const message = req.body;
-    const { email, password } = message;
-
+    const { email, password: pwd } = message;
     console.log("/signup", message);
 
-    //TODO Make a real signup
-    return res.send({
-        token: Date.now()
-    });
-});
-//* ================= </AUTHENTICATION> =========================
-
-
-//* ================= <UPLOAD> =========================
-    // //? An example <FORM> 
-    // <form action={ `http://192.168.86.100:3001/media/upload` } method="post" encType="multipart/form-data">
-    //     <input type="file" name="avatar" />                
-    //     <input type="submit" name="upload-button" value="Upload" />
-    // </form>
-
-    const MULTER_STORAGE = multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, "./data/image");
-        },
-
-        // By default, multer removes file extensions so let"s add them back
-        filename: function (req, file, cb) {
-            cb(null, `${ file.fieldname }-${ Date.now() }${ path.extname(file.originalname) }`);
-        }
-    });
-
-    APP.post("/media/upload", (req, res) => {
-        console.log("/media/upload");
-
-        let upload = multer({
-            storage: MULTER_STORAGE,
-            fileFilter: (req, file, cb) => {
-                // Accept images only
-                if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
-                    req.fileValidationError = "Only image files are allowed!";
-                    return cb(new Error("Only image files are allowed!"), false);
-                }
-                cb(null, true);
-            }
-        }).single("photo");    // ("avatar") is the <input name="avatar" type="file" /> element
-
-        upload(req, res, function (err) {
-            // req.file contains information of uploaded file
-            // req.body contains information of text fields, if there were any
-
-            if (req.fileValidationError) {
-                return res.send(req.fileValidationError);
-            }
-            else if (!req.file) {
-                return res.send("Please select an image to upload");
-            }
-            else if (err instanceof multer.MulterError) {
-                return res.send(err);
-            }
-            else if (err) {
-                return res.send(err);
-            }
-
-            const postFilename = `./data/post/${ req.body.handle.toLowerCase() }.json`;
-            const postData = {
-                id: `${ req.body.handle }-${ Date.now() }`,
-                image: req.file.filename,
-            };
-
-            fs.readFile(postFilename, "utf8", (err, data) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    let posts = JSON.parse(data);
-                    
-                    posts.push(postData);
-    
-                    let json = JSON.stringify(posts);
-                    fs.writeFile(postFilename, json, () => {
-                        return res.send(postData);
+    if(email && pwd) {
+        const hashPwd = crypto.createHash("sha256").update(pwd).digest("hex");
+        
+        DB.query(`CALL SignUp(?, ?)`, [ email, hashPwd ], function (error, [ results ], fields) {
+            if(results[ 0 ].Result) {
+                const token = createToken(email, hashPwd, 60 * 60 * 24);    // 24 Hours
+            
+                if(token) {
+                    return res.send({
+                        token,
                     });
                 }
+            }
+            
+            return res.send({
+                token: false
             });
         });
-    });
-//* ================= </UPLOAD> =========================
-
-
-//* ================= <POSTS> =========================
-    APP.post("/post/react/", (req, res) => {
-        const filepath = "./data/messages.json";
-        const message = req.body;
-        const { pid, emoji, token: user } = message;
-
-        console.log("/post/react/", message);
-
-        fs.readFile(filepath, "utf8", (err, data) => {
-            if (err) {
-                console.log(err);
-            } else {
-                let posts = JSON.parse(data);
-                let resPost;
-                posts.map(post => {
-                    if (post.id === pid) {
-                        post.reactions = post.reactions || [];
-                        post.reactions.push({
-                            emoji,
-                            user
-                        });
-
-                        resPost = {
-                            id: post.id,
-                            reactions: post.reactions
-                        };
-                    }
-                });
-
-                let json = JSON.stringify(posts);
-                fs.writeFile(filepath, json, () => {
-                    res.send(resPost);
-                });
-            }
-        });
-    });
-    APP.post("/post/response/", (req, res) => {
-        const filepath = "./data/messages.json";
-        const message = req.body;
-
-        console.log("/post/response/", message);
-
-        //TODO Save comments to a post
-    });
-//* ================= </POSTS> =========================
-
-APP.get("/album/:aid", (req, res) => {
-    const albumId = req.params.aid;
-    console.log(`/album/${ albumId }`);
-
-    fs.readdir(path.join(__dirname, "/data/image/"), function (err, files) {
-        if(err) {
-            return;
-        } 
-
-        const uris = files.map(file => SERVERS.ALBUM.uri(`/img/${ file }`));
-
-        res.send(uris);
-    });
+    }
 });
 
-APP.get("/user/:handle", (req, res) => {
-    const handle = req.params.handle.toLowerCase();
-    const filename = `./data/user/${ handle }.json`;
-    console.log(`/user/${handle}`);
-
-    fs.readFile(filename, function (err, buff) {
-        return res.send(buff.toString());
-    });
-});
-
-APP.get("/family/:handle", (req, res) => {
-    const handle = req.params.handle.toLowerCase();
-    const filename = `./data/family/${ handle }.json`;
-    console.log(`/family/${handle}`);
-
-    fs.readFile(filename, function (err, buff) {
-        return res.send(buff.toString());
-    });
-});
-
-APP.get("/friends/:handle", (req, res) => {
-    const handle = req.params.handle;
-    const filename = `./data/user/${ handle.toLowerCase() }.json`;
-    console.log(`/friends/${handle}`);
-
-    fs.readFile(filename, "utf8", (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            let profile = JSON.parse(data);
-            
-            return res.send(profile.friends);
-        }
-    });
-});
-
-APP.get("/post/:pid", (req, res) => {
-    const postId = req.params.pid;
-    const filename = `./data/post/${ postId.split("-")[ 0 ].toLowerCase() }.json`;
-    console.log(`/post/${postId}`);
-
-    fs.readFile(filename, "utf8", (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            let posts = JSON.parse(data);
-            let post = posts.filter(post => post.id === postId);
-    
-            if(post) {
-                return res.send(post[ 0 ]);
-            }
-
-            return res.sendStatus(404);
-        }
-    });
-});
-
-APP.get("/feed/:handle", (req, res) => {
-    const handle = req.params.handle;
-    console.log(`/feed/${handle}`);
-
-    fs.readFile(`./data/post/${ handle.toLowerCase() }.json`, function (err, buff) {
-        return res.send(buff.toString());
-    });
-});
-
-APP.get("/image/:iid", (req, res) => {
-    const imageId = "pusheen" || req.params.iid;
-    const imageExt = "png";
-    const filepath = `/data/image/${imageId}.${imageExt}`;
-    console.log(`/image/${imageId}`);
-
-    return res.sendFile(filepath, { root: __dirname });
-});
-
-
-
-APP.get("/test", (req, res) => {
-    console.log(`/test`);
-
-    DB.query("SELECT * FROM test", function (error, results, fields) {
-        return res.send(results)
-    });
-});
 
 APP.listen(PORT, () =>
     console.log(`KiKi Pupus API listening on port ${PORT}!`),
